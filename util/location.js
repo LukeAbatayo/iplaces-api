7ba8e030ff116322c793aa8ab9adbc5814195ab6
Luke Abatayo <luke.abatayo17@gmail.com>
2022-08-06 15:13:33 +0800

    first commit

---
+++ util/location.js
@@ -0,0 +1,43 @@
+// const getCoordsForAddress = async address => {
+//   return {
+//     lat: 40.7484474,
+//     lng: -73.9871516,
+//   };
+// };
+
+// module.exports = getCoordsForAddress;
+
+const axios = require('axios');
+const HttpError = require('../models/http-error');
+const API_KEY = process.env.GOOGLE_API_KEY;
+
+async function getCoordsForAddress(address) {
+  const response = await axios.get(
+    `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${encodeURIComponent(
+      address
+    )}&format=json`
+  );
+
+  const data = response.data[0];
+
+  // console.log(data);
+
+  if (!data || data.status === 'ZERO_RESULTS') {
+    const error = new HttpError(
+      'Could not find location for the specified address.',
+      422
+    );
+    throw error;
+  }
+
+  const coorLat = data.lat;
+  const coorLon = data.lon;
+  const coordinates = {
+    lat: coorLat,
+    lng: coorLon,
+  };
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
